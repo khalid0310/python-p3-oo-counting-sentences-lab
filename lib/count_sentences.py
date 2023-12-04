a481f0b8c7e24d01c70b0f365ce3d7a0d742797e
khalid <khalid.mohamed@moringaschool.com>
2023-12-04 09:46:30 +0300

    complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentence_ends = ['.', '!', '?']
+        sentences = [s.strip() for s in self.value.splitlines() if s.strip()]
+        for end in sentence_ends:
+            sentences = [sentence.split(end) for sentence in sentences]
+            sentences = [part for sublist in sentences for part in sublist]
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
